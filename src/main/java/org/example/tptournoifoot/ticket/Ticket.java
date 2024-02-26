150e3216780aa887c550695d77467553dde29e1d
Anna RAACH <annagraph92@gmail.com>
2024-02-26 15:48:58 +0100

    feat(tp-final) : colomn ticket✨

--- src/main/java/org/example/tptournoifoot/ticket/Ticket.java
+++ src/main/java/org/example/tptournoifoot/ticket/Ticket.java
@@ -1,5 +1,6 @@
 package org.example.tptournoifoot.ticket;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -15,8 +16,16 @@ public class Ticket {
     @Id
     @GeneratedValue
     private Integer id;
+
+    @Column(name = "type")
     private String type;
+
+    @Column(name = "numero")
     private Integer numero;
+
+    @Column(name = "numero_de_range")
     private Integer numeroRange;
+
+    @Column(name = "prix")
     private double prix;
 }
