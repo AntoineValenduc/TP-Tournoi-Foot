0a54832fb3e77cea72deb4f2643c6ce541d287d8
Anna RAACH <annagraph92@gmail.com>
2024-02-26 15:41:40 +0100

    feat(tp-final) : entite supporter ✨

--- src/main/java/org/example/tptournoifoot/supporter/Supporter.java
+++ src/main/java/org/example/tptournoifoot/supporter/Supporter.java
@@ -1,2 +1,20 @@
-package org.example.tptournoifoot.supporter;public class Supporter {
+package org.example.tptournoifoot.supporter;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Supporter {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String nom;
+    private String prenom;
 }
