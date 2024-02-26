ce3273454c97e97166de64ea2e3b743601a08a6e
Anna RAACH <annagraph92@gmail.com>
2024-02-26 15:41:13 +0100

    feat(tp-final) : entite ticket ✨

--- src/main/java/org/example/tptournoifoot/ticket/Ticket.java
+++ src/main/java/org/example/tptournoifoot/ticket/Ticket.java
@@ -1,2 +1,22 @@
-package org.example.tptournoifoot.ticket;public class Ticket {
+package org.example.tptournoifoot.ticket;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Ticket {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String type;
+    private Integer numero;
+    private Integer numeroRange;
+    private double prix;
 }
