4d33b9cd4b04e4889d178a81c701be793888d97d
AntoineValenduc <ant.valenduc@gmail.com>
2024-02-26 14:31:18 +0100

    First commit - init project

---
+++ src/test/java/org/example/tptournoifoot/TpTournoiFootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tptournoifoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTournoiFootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
