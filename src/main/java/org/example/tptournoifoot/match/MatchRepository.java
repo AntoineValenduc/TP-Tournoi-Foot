a61556626c351c4bbd785b0f3125dca55295ce17
NathanManiezPro <Nathanmaniez@gmail.com>
2024-02-28 15:56:31 +0100

    Voila !!!

--- src/main/java/org/example/tptournoifoot/match/MatchRepository.java
+++ src/main/java/org/example/tptournoifoot/match/MatchRepository.java
@@ -2,5 +2,9 @@ package org.example.tptournoifoot.match;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface MatchRepository extends JpaRepository<Match, Integer> {
+    List<Match> findByDate(LocalDate date);
 }
